acccf8677d41ea7ad1a9e9c81ea991287731bd83
Colin Serafino <cserafino66@gmail.com>
2017-10-16 18:04:00 -0400

    Editing React Router to include About path

---
+++ app/javascript/components/about/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SpotlightLayout from '../../layouts/SpotlightLayout';
+
+
+const About = props => (
+	<SpotlightLayout>
+		<div className="about" >
+      <h1>Lorem Ipsum Testy McTestface</h1>
+		</div>
+	</SpotlightLayout>
+);
+
+export default About;
--- app/javascript/routes/index.js
+++ app/javascript/routes/index.js
@@ -3,6 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 
 // Basic routes
 import Home from '../components/home/Home';
+import About from '../components/about/About/About.js';
 
 // GraphiQL browser UI for testing API
 import graphiqlRoutes from '../modules/graphiql/routes';
@@ -14,7 +15,7 @@ export default (
 	<div>
 		{/* Normal home landing page */}
 		<Route exact path="/" component={Home} />
-		
+		<Route path="/about" component={ About } />
 		{/* Routes for GraphiQL */}
 		{graphiqlRoutes}
 	</div>
