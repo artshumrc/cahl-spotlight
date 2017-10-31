a903eec2d9fb1901f4bec45cce8cbc683e64b4b7
Colin Serafino <cserafino66@gmail.com>
2017-10-31 11:29:23 -0400

    cosmetic changes to search filter

--- app/javascript/components/collection/Collection/Collection.js
+++ app/javascript/components/collection/Collection/Collection.js
@@ -32,11 +32,10 @@ const Collection = props => {
 						<div className="search-widgets pull-right">
 							<div id="sort-dropdown" className="btn-group">
 								<button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">
-									Sort by relevance <span className="caret"></span>
+									Sort by... <span className="caret"></span>
 								</button>
 
 								<ul className="dropdown-menu" role="menu">
-									<li><a href="/cahl/browse/">relevance</a></li>
 									<li><a href="/cahl/browse/">year (new to old)</a></li>
 									<li><a href="/cahl/browse/">year (old to new)</a></li>
 									<li><a href="/cahl/browse/">author</a></li>
@@ -55,31 +54,7 @@ const Collection = props => {
 									<li><a href="/cahl/browse/">100<span className="sr-only"> per page</span></a></li>
 								</ul>
 							</div>
-							<div className="view-type">
-								{/* Display types, different JSX components */}
-								<span className="sr-only"></span>
-								<div className="view-type-group btn-group">
-									<a title="List" className="btn btn-default view-type-list " href="/cahl/browse/">
-										<span className="glyphicon glyphicon-list view-icon-list" />
-									</a>
-									<a title="Gallery" className="btn btn-default view-type-gallery " href="/cahl/browse/">
-										<span className="glyphicon glyphicon-gallery view-icon-gallery"></span>
-									</a>
-									<a title="Map" className="btn btn-default view-type-heatmaps " href="/cahl/browse/">
-										<span className="glyphicon glyphicon-heatmaps view-icon-heatmaps"></span>
-									</a>
 
-									{/* redisplay results w/Bricks */}
-									<a title="Masonry" className="btn btn-default view-type-masonry active" href="/cahl/browse/">
-										<span className="glyphicon glyphicon-masonry view-icon-masonry"></span>
-									</a>
-
-									{/* Pagination, 1-by-1 */}
-									<a title="Slideshow" className="btn btn-default view-type-slideshow " href="/cahl/browse/">
-										<span className="glyphicon glyphicon-slideshow view-icon-slideshow"></span>
-									</a>
-								</div>
-							</div>
 						</div>
 					</div>
 					<div id="documents" className="row collectionItems">
